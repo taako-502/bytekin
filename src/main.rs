58bc7bbc20400c5ebd3ca58ec8c55313621d2def
taako-502 <yamamoto.takao502@gmail.com>
2026-08-18 01:55:49 +0900

    関数とテスト追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Bytekin!");
+}
+
+fn calculate_level(xp: u32) -> u32 {
+    xp / 100 + 1
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_level_1_when_xp_is_0() {
+        let actual = calculate_level(0);
+        assert_eq!(actual, 1);
+    }
+
+    #[test]
+    fn level_increases_every_100_xp() {
+        assert_eq!(calculate_level(99), 1);
+        assert_eq!(calculate_level(100), 2);
+        assert_eq!(calculate_level(199), 2);
+        assert_eq!(calculate_level(200), 3);
+    }
 }
